341a87a4ec54e425e323268e852edbc1380e6eb0
Vikas Shambu <vikas@drillbitplagiarism.com>
2026-09-03 16:29:07 +0530

    Rename function to moveZeroes and refactor logic for moving zeros to the end of the array

--- leet_code/move-zero-end.js
+++ leet_code/move-zero-end.js
@@ -1,19 +1,18 @@
-// Move all 0s to the end of list without creating an new array
-function moveNonZeroEnd(arr) {
-  let index = 0;
+function moveZeroes(arr) {
+  let slow = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== 0) {
-      arr[index] = arr[i];
-      index++;
+  for (let fast = 0; fast < arr.length; fast++) {
+    if (arr[fast] !== 0) {
+      arr[slow] = arr[fast];
+      slow++;
     }
   }
 
-  for (let i = index; i < arr.length; i++) {
-    arr[i] = 0;
+  while (slow < arr.length) {
+    arr[slow] = 0;
+    slow++;
   }
-
   return arr;
 }
 
-console.log(moveNonZeroEnd([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0, 1, 0, 3, 12])); // [ 1, 3, 12, 0, 0 ]
